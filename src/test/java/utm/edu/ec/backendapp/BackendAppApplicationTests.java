f7ed0a8aa8d6050d5adbab206fac3764861d5caa
Daniel Arteaga <danielarteagasubiaga@gmail.com>
2022-04-26 15:44:18 -0500

    Commit initial

---
+++ src/test/java/utm/edu/ec/backendapp/BackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package utm.edu.ec.backendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
